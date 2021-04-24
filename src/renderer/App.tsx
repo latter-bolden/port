1e69f3b2be21b62a89327a8e7c33f19c54f3a0a8
Hunter Miller <hunter@thisdot.co>
2021-04-24 12:01:48 -0500

    make sure ships are always up to date

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom'
 import { hot } from 'react-hot-loader';
 import { Welcome } from './pages/Welcome'
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider, useInfiniteQuery, useQuery } from 'react-query';
 import { Launch } from './ship/Launch';
 import { ErrorBoundary } from 'react-error-boundary';
 import { ErrorPage } from './pages/ErrorPage';
@@ -15,6 +15,8 @@ import { Boot } from './ship/Boot';
 import { PlanetDetails } from './details/PlanetDetails';
 import { routeMap } from './routes';
 import create from 'zustand';
+import { pierKey } from './query-keys';
+import { send } from './client/ipc';
 
 const queryClient = new QueryClient();
 
@@ -22,26 +24,43 @@ export const useStore = create(() => ({
     piers: []
 }))
 
+const AppWrapped = () => (
+    <QueryClientProvider client={queryClient}>
+        <HashRouter>
+            <ErrorBoundary fallbackRender={ErrorPage}>
+                <App />
+            </ErrorBoundary>
+        </HashRouter>
+    </QueryClientProvider>
+)
+
 const App = () => {
+    useQuery(pierKey(), async () => {
+        const piers = await send('get-piers')
+        
+        for (const pier of piers) {
+            await send('check-pier', pier);
+        }
+
+        return send('get-piers')
+    }, {
+        refetchInterval: 60 * 1000,
+        refetchIntervalInBackground: true
+    });
+
     return (
-        <QueryClientProvider client={queryClient}>
-            <HashRouter>
-                <ErrorBoundary fallbackRender={ErrorPage}>
-                    <Switch>                        
-                        <Route exact path={routeMap.remote.path} component={RemotePierDetails} />
-                        <Route exact path={routeMap.existing.path} component={ExistingShipDetails} />
-                        <Route exact path={routeMap.planet.path} component={PlanetDetails} />
-                        <Route exact path={routeMap.moon.path} component={MoonDetails} />
-                        <Route exact path={routeMap.comet.path} component={CometDetails} />
-                        <Route path="/boot/:slug" component={Boot} />
-                        <Route exact path="/pier/:slug/launch" component={Launch} />
-                        <Route path="/pier/:slug" component={Ship} />
-                        <Route exact path="/" component={Welcome} />
-                    </Switch>
-                </ErrorBoundary>
-            </HashRouter>
-        </QueryClientProvider>
+        <Switch>                        
+            <Route exact path={routeMap.remote.path} component={RemotePierDetails} />
+            <Route exact path={routeMap.existing.path} component={ExistingShipDetails} />
+            <Route exact path={routeMap.planet.path} component={PlanetDetails} />
+            <Route exact path={routeMap.moon.path} component={MoonDetails} />
+            <Route exact path={routeMap.comet.path} component={CometDetails} />
+            <Route path="/boot/:slug" component={Boot} />
+            <Route exact path="/pier/:slug/launch" component={Launch} />
+            <Route path="/pier/:slug" component={Ship} />
+            <Route exact path="/" component={Welcome} />
+        </Switch>    
     );
 }
 
-export default hot(module)(App);
+export default hot(module)(AppWrapped);
