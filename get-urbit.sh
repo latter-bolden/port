71b8d2168f6c8685481aa6d4a6ce314d91f9f777
Hunter Miller <hunter@thisdot.co>
2021-09-30 14:35:38 -0500

    1.6 binaries, decided on higher version bump

--- get-urbit.sh
+++ get-urbit.sh
@@ -4,14 +4,16 @@ rpath=/nix/store/cx86fgsqp9dkx2h88rq7q73hhh0an9fy-swift-corefoundation/Library/F
 staticpath=/System/Library/Frameworks
 cd resources/mac
 rm urbit*
-curl -JLO https://urbit.org/install/mac/latest
-tar zxvf ./darwin.tgz --strip-components=1 --strip=1
-rm darwin.tgz urbit-king
-install_name_tool -rpath $rpath $staticpath urbit
-install_name_tool -rpath $rpath $staticpath urbit-worker
+# https://urbit.org/install/mac/latest
+curl -JLO https://bootstrap.urbit.org/ci/urbit-v1.6-x86_64-darwin-b1bc4b54f.tgz
+tar zxvf ./urbit-v1.6-x86_64-darwin-b1bc4b54f.tgz --strip-components=1 --strip=1
+rm urbit-*.tgz urbit-king
+# install_name_tool -rpath $rpath $staticpath urbit
+# install_name_tool -rpath $rpath $staticpath urbit-worker
 
 cd ../linux
 rm urbit*
-curl -JLO https://urbit.org/install/linux64/latest
-tar zxvf ./linux64.tgz --strip-components=1 --strip=1
-rm linux64.tgz urbit-king
+# https://urbit.org/install/linux64/latest 
+curl -JLO https://bootstrap.urbit.org/ci/urbit-v1.6-x86_64-linux-b1bc4b54f.tgz
+tar zxvf ./urbit-v1.6-x86_64-linux-b1bc4b54f.tgz --strip-components=1 --strip=1
+rm urbit-*.tgz urbit-king
