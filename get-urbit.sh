bb6f30882fbd07e5a6a6cbb2eb27a5b9244a07fa
Hunter Miller <hunter@thisdot.co>
2021-09-30 14:52:32 -0500

    still need to run static paths

--- get-urbit.sh
+++ get-urbit.sh
@@ -8,8 +8,8 @@ rm urbit*
 curl -JLO https://bootstrap.urbit.org/ci/urbit-v1.6-x86_64-darwin-b1bc4b54f.tgz
 tar zxvf ./urbit-v1.6-x86_64-darwin-b1bc4b54f.tgz --strip-components=1 --strip=1
 rm urbit-*.tgz urbit-king
-# install_name_tool -rpath $rpath $staticpath urbit
-# install_name_tool -rpath $rpath $staticpath urbit-worker
+install_name_tool -rpath $rpath $staticpath urbit
+install_name_tool -rpath $rpath $staticpath urbit-worker
 
 cd ../linux
 rm urbit*
