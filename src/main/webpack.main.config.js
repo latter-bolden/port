a88ab9d3d9f91625c0d6005df69fb8429c5ad7f5
Hunter Miller <hunter@thisdot.co>
2021-10-27 22:54:07 -0500

    only worry about conpty if win

--- src/main/webpack.main.config.js
+++ src/main/webpack.main.config.js
@@ -1,12 +1,18 @@
+const os = require('os');
+const entry = {
+  index: './src/main/index.ts',
+}
+
+if (os.platform() === 'win32') {
+  entry.conpty_console_list_agent = './node_modules/node-pty/lib/conpty_console_list_agent.js';
+}
+
 module.exports = {
   /**
    * This is the main entry point for your application, it's the first file
    * that runs in the main process.
    */
-  entry: {
-    index: './src/main/index.ts', 
-    conpty_console_list_agent: './node_modules/node-pty/lib/conpty_console_list_agent.js',
-  },
+  entry,
   // Put your normal webpack config below here
   module: {
     rules: require('../../webpack.rules'),
