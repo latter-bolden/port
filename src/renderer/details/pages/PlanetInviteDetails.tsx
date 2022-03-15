928d52102818f6d83121cbe7639fccb871086273
Logan Allen <loganallc@gmail.com>
2022-03-15 17:03:47 -0500

    PlanetInviteDetails: replace iframe with an external link as the content security policy blocks

--- src/renderer/details/pages/PlanetInviteDetails.tsx
+++ src/renderer/details/pages/PlanetInviteDetails.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { send } from '../../client/ipc'
 import { LeftArrow } from '../../icons/LeftArrow'
+import { RightArrow } from '../../icons/RightArrow'
 import { Layout } from '../../shared/Layout'
 import { Link, useParams } from 'react-router-dom'
 
@@ -8,9 +9,9 @@ import { Link, useParams } from 'react-router-dom'
 export const PlanetInviteDetails: React.FC = () => {
   let params = useParams();
 
-  let url = "https://bridge-dev.urbit.org/#";
+  let url = "https://bridge.urbit.org/#";
   if (params['ticket'] === ':ticket') {
-    url = url + 'sampel-palnet-sampel-palnet';
+    url = url + 'sampel-palnet-sampel-palnet-sampel-palnet';
   } else {
     url = url + params['ticket'];
   }
@@ -26,12 +27,12 @@ export const PlanetInviteDetails: React.FC = () => {
             <LeftArrow className="w-7 h-7" primary="fill-current text-white dark:text-black" secondary="fill-current text-gray-200 dark:text-gray-700 group-hover:text-gray-300 dark:group-hover:text-gray-600 group-focus:text-gray-300 dark:group-focus:text-gray-600 transition-colors"/>
             <span className="sr-only">Back</span>
         </Link>
-        <div className="w-full h-full bg-white" style={{ borderRadius: '1rem', overflow: 'hidden', maxWidth: '48rem' }}>
-          <iframe style={{ position: "relative", width: "100%", height:"100%" }}
-                  src={url} />
+        <div className="w-full h-full flex items-center justify-center p-12" style={{ maxWidth: '48rem', textAlign: 'center', flexDirection: 'column' }}>
+          <a target="_blank" href={url}>Claim your Planet in Bridge</a>
+          <p>Once you have finished, click the arrow on the right.</p>
         </div>
-        <Link to="/boot/planet" className="group focus:outline-none no-underline mr-12" style={{ opacity: '0' }}>
-            <LeftArrow className="w-7 h-7" primary="fill-current text-white dark:text-black" secondary="fill-current text-gray-200 dark:text-gray-700 group-hover:text-gray-300 dark:group-hover:text-gray-600 group-focus:text-gray-300 dark:group-focus:text-gray-600 transition-colors"/>
+        <Link to="/boot/planet/key" className="group focus:outline-none no-underline mr-12">
+            <RightArrow className="w-7 h-7" primary="fill-current text-white dark:text-black" secondary="fill-current text-gray-200 dark:text-gray-700 group-hover:text-gray-300 dark:group-hover:text-gray-600 group-focus:text-gray-300 dark:group-focus:text-gray-600 transition-colors"/>
             <span className="sr-only">Back</span>
         </Link>
       </div>
