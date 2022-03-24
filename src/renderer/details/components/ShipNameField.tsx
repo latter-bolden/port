607d7a4e372160bd79048cf03239c2bc3ab2be24
latter bolden <latter-bolden@protonmail.com>
2022-03-24 14:12:29 -0500

    add existing ship detection to ShipNameField

--- src/renderer/details/components/ShipNameField.tsx
+++ src/renderer/details/components/ShipNameField.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useQuery } from 'react-query';
+import { Link } from 'react-router-dom'
 import { UseFormMethods, Validate } from 'react-hook-form/dist/types'
-import { AddPier } from '../../../background/services/pier-service'
+import { Pier, AddPier } from '../../../background/services/pier-service'
+import { send } from '../../client/ipc';
+import { pierKey } from '../../query-keys';
 
 interface ShipNameFieldProps {
     form: UseFormMethods<AddPier>;
@@ -9,8 +13,14 @@ interface ShipNameFieldProps {
 }
 
 export const ShipNameField: React.FC<ShipNameFieldProps> = ({ form, validator, placeholder = '~sampel-palnet' }) => {
+    const { data: piers } = useQuery(pierKey(), () => send('get-piers'))
     const shipnamePattern = /^[a-z~-]*$/i;
     const shipnameContainsInvalidCharacters = form.errors.shipName?.type === 'pattern';
+
+    function getMatchingPier(shipName: string): Pier | undefined {
+        return piers.find(pier => pier.shipName.trim().toLowerCase() === shipName.toLowerCase())
+    }
+    const shipNameValidator = (value: string) => !getMatchingPier(value)
 
     return (
         <>
@@ -21,7 +31,7 @@ export const ShipNameField: React.FC<ShipNameFieldProps> = ({ form, validator, p
                 ref={form.register({ 
                     required: true,
                     pattern: shipnamePattern,
-                    validate: validator,
+                    validate: validator ? validator : shipNameValidator,
                     maxLength: 28
                 })}
                 className="input flex w-full mt-2" 
@@ -32,6 +42,11 @@ export const ShipNameField: React.FC<ShipNameFieldProps> = ({ form, validator, p
                 { form.errors.shipName?.type === 'required' && 'Ship name is required'}
                 { form.errors.shipName?.type === 'maxLength' && 'Ship name must be 28 characters or less'}
                 { (!form.errors.shipName || shipnameContainsInvalidCharacters) && 'Ship name must only contain alphanumeric, dash, underscore, or tilde characters' }
+                { form.errors.shipName?.type === 'validate' && 
+                    <span>
+                        A ship with this name <Link to={`/pier/${getMatchingPier(form.getValues().shipName).slug}`}>is already docked</Link> in Port.
+                    </span>
+                }
             </span>
         </>
     )
