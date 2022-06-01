01c5d654c0544e327849aa42c2de87603f8edd46
Hunter Miller <hunter@hmiller.dev>
2022-06-01 00:07:21 -0500

    binaries: upgrading for 1.9 release

--- get-urbit.bat
+++ get-urbit.bat
@@ -1,5 +1,6 @@
 cd resources/win
 del urbit*
-curl -JLO https://github.com/urbit/urbit/releases/latest/download/windows.tgz
-tar zxvf ./windows.tgz --strip-components=1 --strip=1
-del windows.tgz
+curl -JLO https://bootstrap.urbit.org/vere/soon/1.9-rc6/vere-v1.9-rc6-x86_64-windows.exe
+@REM tar zxvf ./windows.tgz --strip-components=1 --strip=1
+@REM del windows.tgz
+MOVE vere-v1.9-rc6-x86_64-windows.exe urbit.exe
--- get-urbit.sh
+++ get-urbit.sh
@@ -4,14 +4,13 @@ rpath=/nix/store/cx86fgsqp9dkx2h88rq7q73hhh0an9fy-swift-corefoundation/Library/F
 staticpath=/System/Library/Frameworks
 cd resources/mac
 rm urbit*
-curl -JLO https://urbit.org/install/mac/latest
-tar zxvf ./darwin.tgz --strip-components=1 --strip=1
-rm darwin.tgz urbit-king
+curl -JLO https://bootstrap.urbit.org/urbit-v1.9-rc3-darwin.tgz
+tar zxvf ./urbit-v1.9-rc3-darwin.tgz --strip-components=1 --strip=1
+rm urbit-v1.9-rc3-darwin.tgz
 install_name_tool -rpath $rpath $staticpath urbit
-install_name_tool -rpath $rpath $staticpath urbit-worker
 
 cd ../linux
 rm urbit*  
-curl -JLO https://urbit.org/install/linux64/latest
-tar zxvf ./linux64.tgz --strip-components=1 --strip=1
-rm linux64.tgz urbit-king
+curl -JLO https://bootstrap.urbit.org/urbit-v1.9-rc3-linux64.tgz
+tar zxvf ./urbit-v1.9-rc3-linux64.tgz --strip-components=1 --strip=1
+rm urbit-v1.9-rc3-linux64.tgz
