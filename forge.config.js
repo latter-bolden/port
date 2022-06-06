e1711d52d2d1e236b0a37b9210509e247f24e9a2
~latter-bolden <latter-bolden@protonmail.com>
2022-06-06 18:10:04 -0400

    try forked flatpak maker

--- forge.config.js
+++ forge.config.js
@@ -93,7 +93,7 @@ module.exports = {
       ]
     },
     {
-      name: '@electron-forge/maker-flatpak',
+      name: 'maker-flatpak',
       config: {
         options: {
           id: 'org.urbit.port',
