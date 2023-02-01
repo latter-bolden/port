2180b65cf7aca7542a3c2104125c21fd325ce2a4
Alex Shelkovnykov <a@ashelkov.com>
2023-02-01 12:29:14 +0100

    ops: update for binary v1.18

--- get-urbit.sh
+++ get-urbit.sh
@@ -2,12 +2,18 @@
 
 cd resources/mac
 rm urbit*
-curl -JLO https://urbit.org/install/mac/latest
-tar zxvf ./darwin.tgz --strip-components=1 --strip=1
-rm darwin.tgz
+curl -JLO https://urbit.org/install/macos-x86_64/latest
+tar -xzvf macos-x86_64.tgz
+rm macos-x86_64.tgz
+
+cd resources/mac
+rm urbit*
+curl -JLO https://urbit.org/install/macos-aarch64/latest
+tar -xzvf macos-aarch64.tgz
+rm macos-aarch64.tgz
 
 cd ../linux
 rm urbit*  
-curl -JLO https://urbit.org/install/linux64/latest
-tar zxvf ./linux64.tgz --strip-components=1 --strip=1
-rm linux64.tgz
+curl -JLO https://urbit.org/install/linux-x86_64/latest
+tar -xzvf linux-x86_64.tgz
+rm linux-x86_64.tgz
