8891043066f8b9f38e66e05e636dc1854a2dcd7c
~latter-bolden <latter-bolden@protonmail.com>
2023-02-06 17:17:32 -0500

    remove any old urbit/vere(s) and add download for linux aarch64

--- get-urbit.sh
+++ get-urbit.sh
@@ -2,18 +2,24 @@
 
 cd resources/mac
 rm urbit*
+rm vere*
+
 curl -JLO https://urbit.org/install/macos-x86_64/latest
 tar -xzvf macos-x86_64.tgz
 rm macos-x86_64.tgz
 
-cd resources/mac
-rm urbit*
 curl -JLO https://urbit.org/install/macos-aarch64/latest
 tar -xzvf macos-aarch64.tgz
 rm macos-aarch64.tgz
 
 cd ../linux
-rm urbit*  
+rm urbit*
+rm vere*
+
 curl -JLO https://urbit.org/install/linux-x86_64/latest
 tar -xzvf linux-x86_64.tgz
 rm linux-x86_64.tgz
+
+curl -JLO https://urbit.org/install/linux-aarch64/latest
+tar -xzvf linux-aarch64.tgz
+rm linux-aarch64.tgz
